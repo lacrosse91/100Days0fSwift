1d83c96251ca8180e845ea8def8076708981b706
川内翔一朗 <>
2019-10-27 21:29:49 +0900

    first commit

---
+++ SwiftUI100days/SwiftUI100days/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI100days
+//
+//  Created by 川内翔一朗 on 2019/10/27.
+//  Copyright © 2019 川内翔一朗. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
