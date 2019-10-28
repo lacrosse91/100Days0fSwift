354af9979e70bd836e8bddee8ca65ef0efc33e88
川内翔一朗 <>
2019-10-28 09:07:25 +0900

    create picker

--- SwiftUI100days/SwiftUI100days/ContentView.swift
+++ SwiftUI100days/SwiftUI100days/ContentView.swift
@@ -9,22 +9,35 @@
 import SwiftUI
 
 struct ContentView: View {
-     @State private var checkAmount = ""
-     @State private var numberOfPeople = 2
-     @State private var tipPercentage = 2
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 2
 
     let tipPercentages = [10, 15, 20, 25, 0]
 
-     var body: some View {
-         Form {
-             TextField("Amount", text: $checkAmount)
-             .keyboardType(.decimalPad)
+    var body: some View {
 
-             Section {
-                 Text("$\(checkAmount)")
-             }
-         }
-     }
+        NavigationView {
+
+            Form {
+                Section {
+                    TextField("Amount", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2 ..< 100) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+
+                Section {
+                    Text("$\(checkAmount)だよ")
+                }
+            }
+        .navigationBarTitle("WeSplit")
+        }
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
